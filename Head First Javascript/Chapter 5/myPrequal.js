7d81a67bbfb87c523d1e16468da837faa9011c13
Boyca007 <alex.k.boyce@gmail.com>
2020-09-06 08:40:23 -0400

    Add files via upload

---
+++ Head First Javascript/Chapter 5/myPrequal.js
@@ -0,0 +1,13 @@
+var cadi = {
+    make: "GM",
+    model: "Cadillac",
+    year: 1995,
+    color: "tan",
+    passengars: 5,
+    convertible: false,
+    mileage: 12892
+}
+
+var worthALook = prequal(cadi);
+
+console.log(worthALook);
